67059defcf54591fe189918919fce7fc5bb33d73
Purvesh Jambhulkar <130358075+purveshjambhulkar@users.noreply.github.com>
2025-01-25 11:47:23 +0530

    sorting1

---
+++ 01.Sorting/03.Insertion_Sort.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Please change the array in-place
+    void insertionSort(vector<int>& arr) {
+        // code here
+        
+        //take a element and place it at its correct pos
+        
+        for(int i = 0 ; i < arr.size() ; i++){
+            int j = i ;
+            while(j > 0 && arr[j-1] > arr[j]){
+                swap(arr[j-1] , arr[j]);
+                j--;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        obj.insertionSort(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
