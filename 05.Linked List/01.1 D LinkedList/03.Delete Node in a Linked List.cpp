9dba6da1b4664206a3d08b389552b9502b973dbd
Purvesh Jambhulkar <130358075+purveshjambhulkar@users.noreply.github.com>
2025-03-23 12:21:33 +0530

    dsa

---
+++ 05.Linked List/01.1 D LinkedList/03.Delete Node in a Linked List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ class Solution {
+    public:
+        void deleteNode(ListNode* node) {
+            // Approach
+    
+            while (true) {
+    
+                // check if its second last node
+                if (node->next->next == NULL) {
+                    node->val = node->next->val;
+                    node->next = NULL;
+                    return;
+                }
+    
+                node->val = node->next->val;
+                node = node ->next;
+            }
+        }
+    };
