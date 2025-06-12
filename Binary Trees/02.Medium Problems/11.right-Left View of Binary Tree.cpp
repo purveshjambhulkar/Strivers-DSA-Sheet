e461ab48e84907a69edd3a3f754c85f4174bf26d
Purvesh Jambhulkar <130358075+purveshjambhulkar@users.noreply.github.com>
2025-06-12 11:31:28 +0530

    dsa

---
+++ Binary Trees/02.Medium Problems/11.right-Left View of Binary Tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) {
+            return {};
+        }
+
+        vector<int> result;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                if (temp->left) {
+                    q.push(temp->left);
+                }
+                if (temp->right) {
+                    q.push(temp->right);
+                }
+
+                if (i == size - 1) {
+                    result.push_back(temp->val);
+                }
+            }
+        }
+
+        return result;
+    }
+};
