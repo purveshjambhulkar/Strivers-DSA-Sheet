f282f6acab399ad8f6f3e7352525154396a30ca6
Purvesh Jambhulkar <130358075+purveshjambhulkar@users.noreply.github.com>
2025-06-12 11:42:06 +0530

    dsa

--- Binary Trees/02.Medium Problems/11.right-Left View of Binary Tree.cpp
+++ Binary Trees/02.Medium Problems/11.right-Left View of Binary Tree.cpp
@@ -12,34 +12,58 @@
  */
 class Solution {
 public:
+    
+    void revPreorderUtil(TreeNode* root , int level , vector<int> &result){
+
+        if(root == NULL){
+            return;
+        }
+
+        if(level == result.size()){
+            result.push_back(root->val);
+        }
+
+        revPreorderUtil(root->right , level+ 1 , result);
+        revPreorderUtil(root->left , level+ 1 , result);
+
+    }
+
     vector<int> rightSideView(TreeNode* root) {
-        if (!root) {
-            return {};
-        }
 
+        //Approach 1 - Iterative Traversal
+        // if (!root) {
+        //     return {};
+        // }
+
+        // vector<int> result;
+        // queue<TreeNode*> q;
+        // q.push(root);
+
+        // while (!q.empty()) {
+        //     int size = q.size();
+
+        //     for (int i = 0; i < size; i++) {
+        //         TreeNode* temp = q.front();
+        //         q.pop();
+        //         if (temp->left) {
+        //             q.push(temp->left);
+        //         }
+        //         if (temp->right) {
+        //             q.push(temp->right);
+        //         }
+
+        //         if (i == size - 1) {
+        //             result.push_back(temp->val);
+        //         }
+        //     }
+        // }
+
+        // return result;
+
+
+        //Approach 2 - Recursive Traversal
         vector<int> result;
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
-                if (temp->left) {
-                    q.push(temp->left);
-                }
-                if (temp->right) {
-                    q.push(temp->right);
-                }
-
-                if (i == size - 1) {
-                    result.push_back(temp->val);
-                }
-            }
-        }
-
+        revPreorderUtil(root , 0, result);
         return result;
     }
 };
