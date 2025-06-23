89067392aa47c48d45b9664797286e6e84e6031a
Purvesh Jambhulkar <130358075+purveshjambhulkar@users.noreply.github.com>
2025-06-23 13:08:26 +0530

    dsa

---
+++ 14.Graphs/03.Topo Sort and Problems/01.Topo Sort.cpp
@@ -0,0 +1,48 @@
+class Solution {
+  public:
+  
+    void dfs( vector<vector<int>> &adj ,vector<int> &visited,stack<int> &st , int node ){
+        
+        visited[node] = 1;
+        
+        for(auto n : adj[node]){
+            if(!visited[n]){
+                dfs(adj , visited , st, n);
+            }
+        }
+        
+        st.push(node);
+        
+        
+    }
+  
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>> adj(V);
+        for(auto edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            
+            adj[u].push_back(v);
+        }
+        
+        vector<int> visited(V , 0);
+        stack<int> st;
+        
+        for(int i = 0 ; i < V ; i++){
+            if(!visited[i]){
+                dfs(adj , visited , st, i);
+            }
+        }
+        
+        
+        vector<int> topo;
+        while(!st.empty()){
+            topo.push_back(st.top());
+            st.pop();
+        }
+        
+        return topo;
+        
+    }
+};
